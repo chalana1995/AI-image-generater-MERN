a0cbab5b6b1d1138d5db05059e20a1c2080b4978
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-04-25 18:59:23 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]"></header>
+    </BrowserRouter>
+  );
 };
 
 export default App;
