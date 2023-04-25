11519ddd416b748acd56257aa80adcd7f2f311ab
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-04-25 19:30:10 +0530

    code refacter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Home, CreatePost } from "./pages";
-import { swatch } from "./assets";
+import { logo } from "./assets";
 
 const App = () => {
   return (
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-          <img src={swatch} className="w-28 h-14 object-contain" />
+          <img src={logo} className="w-28 object-contain" />
         </Link>
         <Link
           to="/create-post"
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,9 @@
-import ai from "./ai.png";
-import fileIcon from "./file.png";
-import swatch from "./swatch.png";
-import download from "./download.png";
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
 
-import logoShirt from "./logo-tshirt.png";
-import stylishShirt from "./stylish-tshirt.png";
-
-export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
+export {
+  download,
+  logo,
+  preview,
+};
