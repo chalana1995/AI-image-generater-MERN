1a0c5b115eb70bba577713577ea0e986d9b96a7f
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-05-06 19:44:08 +0530

    rendercars set

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Loader, Card, FormFiled } from "../components";
 
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
+  );
+};
+
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
@@ -36,6 +46,13 @@ const Home = () => {
                 <span className="text-[#222328]">{searchText}</span>
               </h2>
             )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards data={[]} title="No Search results found" />
+              ) : (
+                <RenderCards data={[]} title="No posts found" />
+              )}
+            </div>
           </>
         )}
       </div>
