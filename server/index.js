e8764e9b1e28b266e2edb3aab7bcf54746094781
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-05-13 20:49:29 +0530

    routing basic things done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoute.js";
 
 import connectDB from "./mongodb/connect.js";
 
@@ -9,6 +11,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
